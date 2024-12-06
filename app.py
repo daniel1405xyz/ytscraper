cdf779b64aeaead3dc210c56e3d705f7b501a1cd
daniel1405xyz <133959686+daniel1405xyz@users.noreply.github.com>
2024-12-06 08:33:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -38,4 +38,6 @@ def get_transcript():
             os.remove(filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
